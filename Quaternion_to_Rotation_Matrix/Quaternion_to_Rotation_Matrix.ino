8ed8ab41e88e3d531388e3b5312416936392fdbd
AlexCrownshaw <alex.crownshaw12@gmail.com>
2020-12-22 23:06:54 +0000

    Normalises Quaternion First

--- Quaternion_to_Rotation_Matrix/Quaternion_to_Rotation_Matrix.ino
+++ Quaternion_to_Rotation_Matrix/Quaternion_to_Rotation_Matrix.ino
@@ -4,7 +4,8 @@ using namespace BLA;
 
 double accX {1}, accY {2}, accZ {3};
 double accN {}, accE {}, accD {};
-double qW {0.3058876}, qX {0.3823596}, qY {0.5353034}, qZ {0.6882472};
+double qW {1}, qX {0.5}, qY {4.3}, qZ {0.68};
+double qN {};
 
 BLA::Matrix<3> accBodyVector;
 BLA::Matrix <3> accNEDVector;
@@ -25,7 +26,23 @@ void loop() {
     Serial1.println(accBodyVector(i));
   }
   Serial1.println("=================================================");
-  
+
+  qN = sqrt((qW * qW) + (qX * qX) + (qY * qY)+ (qZ * qZ));
+  qW /= qN;
+  qX /= qN;
+  qY /= qN;
+  qZ /= qN;
+
+  Serial1.println("Print Normalised Quaternion");
+  Serial1.print(qW, 6);
+  Serial1.print("\t");
+  Serial1.print(qX, 6);
+  Serial1.print("\t");
+  Serial1.print(qY, 6);
+  Serial1.print("\t");
+  Serial1.println(qZ, 6);
+  Serial1.println("=================================================");
+
   rotMatrix(0,0) = (1 - (2 * qY * qY) - (2 * qZ * qZ)); rotMatrix(0,1) = ((2 * qX * qY) - (2 * qZ * qW)); rotMatrix(0,2) = ((2* qX * qZ) + (2 * qY * qW));
   rotMatrix(1,0) = ((2 * qX * qY) + (2 * qZ * qW)); rotMatrix(1,1) = (1 - (2 * qX * qX) - (2 * qZ *qZ));  rotMatrix(1,2) = ((2 * qY * qZ) - (2 * qX * qW));
   rotMatrix(2,0) = ((2 * qX * qZ) - (2 * qY * qW)); rotMatrix(2,1) = ((2 * qY * qZ) + (2 * qX * qW)); rotMatrix(2,2) = (1 - (2 * qX * qX) - (2 * qY * qY));
